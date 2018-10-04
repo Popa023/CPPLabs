bb584dbd33cac2b0587841275695a4aec094b83b
Razvan Popescu <popa021@gmail.com>
2018-10-04 18:20:39 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/CPPLabs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPPLabs.dir/main.cpp.o"
+  "CPPLabs.pdb"
+  "CPPLabs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPPLabs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
